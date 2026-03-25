29246a715faea37ccdfd230d671871fa4dfae589
Pheetomilechi Ghajiga <tomilechighajiga@gmail.com>
2026-03-25 15:25:40 +0000

    Navigated to the server dir and ran it on PORT 3000

--- Project 2/project2.js
+++ Project 2/project2.js
@@ -3,7 +3,7 @@ const crypto = require('crypto');
 const app = express();
 const PORT = 3000;
 app.get('/fast', (req, res) => {
-    res.send('Fast response')
+    res.send('Fast response');
 });
 function heavyTask() {
     crypto.pbkdf2Sync("password", "salt", 1000000, 64, "sha512");
