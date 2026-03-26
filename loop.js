c7f7e56478a41aae1dd7a26d652286f62e8b8b00
Pheetomilechi Ghajiga <tomilechighajiga@gmail.com>
2026-03-26 12:46:24 +0000

    Wrote some whiloe loop execution guides on the loop.js

--- loop.js
+++ loop.js
@@ -16,6 +16,13 @@ function shouldContinue() {
 
 //Entire body executes in one 'tick'
 while(shouldContinue) {
+    // 1) Node looks at pendingTimers and sees if any functions are ready to be called
+    // 2) Node looks at pendingOSTasks and pendingOperations and calls relevant callbacks
+    // 3) Pause execution. Continue when... - a new pendingOSTasks is done
+    // - a new pendingOperation is done
+    // - a timer is about to complete
+    // 4) Look at pendingTimers. Call any setImmediate
+    // 5) Handle any 'close events'  
     
 }
 
