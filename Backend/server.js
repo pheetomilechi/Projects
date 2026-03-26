95560bf746883414f8228e4c597f3ef8e19f4512
Pheetomilechi Ghajiga <tomilechighajiga@gmail.com>
2026-03-26 10:20:14 +0000

    created a new server.js file and installed node modules

---
+++ Backend/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const crypto = require('crypto');
+const app = express();
+const PORT = 3000;
+app.get('/fast', (req, res) => {
+    res.send('Fast response');
+});
+function heavyTask() {
+    crypto.pbkdf2Sync("password", "salt", 1000000, 64, "sha512");
+}
+app.get('/slow',(req,res) => {
+    heavyTask();
+    res.send('Slow response');
+});
+app.listen(PORT => {
+    console.log(`Server running on ${PORT}`)
+});
