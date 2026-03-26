f596229cd52515292e77c6f6f5616d6c8721c6c3
Pheetomilechi Ghajiga <tomilechighajiga@gmail.com>
2026-03-26 14:21:05 +0000

    Working on the threadpool by passing hashing to different stacks in the system

--- threads.js
+++ threads.js
@@ -1,3 +1,5 @@
+process.env.UV_THREADPOOL_SIZE = 2;
+
 const crypto = require('crypto');
 
 const start = Date.now();
@@ -6,4 +8,13 @@ crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
 }); 
 crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
     console.log('2:', Date.now() - start);
+}); 
+crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+    console.log('3:', Date.now() - start);
+}); 
+crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+    console.log('4:', Date.now() - start);
+}); 
+crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+    console.log('5:', Date.now() - start);
 }); 
