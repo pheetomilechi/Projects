3dd103ce8bdf1af98413a8dfa5002a94ef863fc8
pheetomilechi <tomilechighajiga@gmail.com>
2026-03-31 11:41:58 +0100

    Install the express node module

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-const fs = require('node:fs');
-//the callback function is the last
-fs.readFile('example.txt', 'utf8', (err, data) => {
-    if(err) {
-        console.error('Error reading file:', err.message);
-        return;
-    }
-    console.log('File content:', data);
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello there');
 });
+app.listen(3000);
